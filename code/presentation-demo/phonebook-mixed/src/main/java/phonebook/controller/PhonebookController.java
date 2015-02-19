de693814c01f12fc58a68e7088a443a84d0e76d6
Daniel Somerfield <dsomerfi@thoughtworks.com>
2015-02-18 21:54:11 -0800

    Phonebook controller all implemented.

--- code/presentation-demo/phonebook-mixed/src/main/java/phonebook/controller/PhonebookController.java
+++ code/presentation-demo/phonebook-mixed/src/main/java/phonebook/controller/PhonebookController.java
@@ -1,13 +1,23 @@
 package phonebook.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import phonebook.domain.PhonebookEntry;
+import phonebook.repository.PhonebookEntryRepository;
 
 import java.util.stream.Stream;
 
 @Controller
 public class PhonebookController {
+
+    private PhonebookEntryRepository repository;
+
+    @Autowired
+    public PhonebookController(final PhonebookEntryRepository repository) {
+        this.repository = repository;
+    }
+
     public Stream<PhonebookEntry> entries() {
-        throw new UnsupportedOperationException(); // TODO
+        return repository.all();
     }
 }
--- code/presentation-demo/phonebook-mixed/src/test/java/phonebook/controller/PhonebookControllerIntTest.java
+++ code/presentation-demo/phonebook-mixed/src/test/java/phonebook/controller/PhonebookControllerIntTest.java
@@ -2,19 +2,18 @@ package phonebook.controller;
 
 import com.mongodb.DB;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import phonebook.IntTest;
 import phonebook.domain.PhonebookEntry;
 import phonebook.repository.PhonebookEntryRepository;
 
-import java.util.stream.Stream;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
-@Ignore("Not yet implemented.")
 public class PhonebookControllerIntTest extends IntTest {
 
     @Autowired
@@ -35,9 +34,9 @@ public class PhonebookControllerIntTest extends IntTest {
     public void testEntryList() {
         final PhonebookEntry entry = PhonebookEntry.create("last-name", "first-name", "email@example.com");
         phonebookEntryRepository.save(entry);
-        Stream<PhonebookEntry> entries = phonebookController.entries();
-        assertThat(entries.count(), is(1));
-        assertThat(entries.findFirst(), is(entry));
+        List<PhonebookEntry> entries = phonebookController.entries().collect(Collectors.toList());
+        assertThat(entries.size(), is(1));
+        assertThat(entries.get(0), is(entry));
     }
 
 }
