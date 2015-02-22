4e03332cfd96e409e6a6c1117b772d9448318ca8
Daniel Somerfield <dsomerfi@thoughtworks.com>
2015-02-22 09:50:54 -0800

    Added first js controller with tests.

--- code/presentation-demo/phonebook-mixed/src/main/webapp/static/js/controllers.js
+++ code/presentation-demo/phonebook-mixed/src/main/webapp/static/js/controllers.js
@@ -1,5 +1,7 @@
 var phonebookAppControllers = angular.module('phonebookAppControllers', []);
 
 phonebookAppControllers.controller('PhonebookController', ['$scope', '$http', function ($scope, $http) {
-
+    $http.get("/services/entries").then(function(result){
+        $scope.phonebookEntries = result.data;
+    });
 }]);
--- code/presentation-demo/phonebook-mixed/src/test/js/specs/PhonebookController.spec.js
+++ code/presentation-demo/phonebook-mixed/src/test/js/specs/PhonebookController.spec.js
@@ -1,19 +1,33 @@
-//DISABLED
-xdescribe('PhonebookController', function(){
+describe('PhonebookController', function () {
 
     var scope;
     var controller;
+    var httpBackend;
+
+    var entryResults = [{
+        "id": "123",
+        "lastName": "last-name",
+        "firstName": "first-name",
+        "emailAddress": "email@example.com"
+    }];
 
     beforeEach(module('phonebookApp'));
 
-    beforeEach(inject(function($rootScope, $controller, $httpBackend) {
+    beforeEach(inject(function ($rootScope, $controller, $httpBackend, $http) {
         scope = $rootScope.$new();
-        controller = $controller('PhonebookController', {$scope: scope, $http:$httpBackend});
+        httpBackend = $httpBackend;
+        controller = $controller('PhonebookController', {$scope: scope, $http: $http});
     }));
 
-    it('contains expected phonebook entries', function() {
-        expect(scope.phonebookEntries).toBeDefined();
-        expect(scope.phonebookEntries.length).toBe(1);
+    it('contains no entries when service has returned none', function () {
+        httpBackend.when("GET", "/services/entries").respond([]);
+        httpBackend.flush();
+        expect(scope.phonebookEntries).toEqual([]);
     });
 
+    it('contains entries service has returned', function () {
+        httpBackend.when("GET", "/services/entries").respond(entryResults);
+        httpBackend.flush();
+        expect(scope.phonebookEntries).toEqual(entryResults);
+    });
 });
